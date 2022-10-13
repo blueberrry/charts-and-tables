a4e4de0e9132b131fff8af11dae310bb954f6b6f
Sean Roughton <sean.roughton@dronecloud.io>
2022-10-13 14:20:29 +0100

    added tailwind, replaced google charts with recharts + tanstack tables

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,24 @@
-export {};
+export type PieChartCell = {
+  name: string;
+  value: number;
+};
+
+export type PieChartData = Array<PieChartCell>;
+
+export interface PieChartProps {
+  data: PieChartData;
+  title: string;
+}
+
+// copied from recharts types
+export interface PieRenderLabelProps {
+  cx?: number | string;
+  cy?: number | string;
+  startAngle?: number;
+  endAngle?: number;
+  paddingAngle?: number;
+  innerRadius?: number | string;
+  outerRadius?: number | string;
+  cornerRadius?: number | string;
+  index?: number;
+}
