5c0ac6b338dd0ab4c4461d833e11055b392a731e
seanroughton <seanroughton@gmail.com>
2022-10-20 14:44:38 +0100

    table change

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,16 +1,7 @@
 import React, { FC, ReactElement, useMemo } from 'react';
 import ReactTableUI from 'react-table-ui';
-import type { DataType, TableInstance } from 'react-table-ui';
 
+// TODO: Prop types
 export function Table({ title, columns, rows }: any): ReactElement {
-  // TODO: Usememo/types
-
-  // const data: User[] = [
-  //   { name: 'Abc Xyx', age: 20 },
-  //   { name: 'Def Uvw', age: 25 },
-  //   { name: 'Ghi Rst', age: 23 },
-  //   { name: 'Jklm Nopq', age: 30 },
-  // ];
-
   return <ReactTableUI data={rows} title={title} />;
 }
